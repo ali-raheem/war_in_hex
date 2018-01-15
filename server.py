1812f86037529bae87a3c2a4f833b19159ed911c
Ali Raheem <ali.raheem@gmail.com>
2018-01-15 01:07:09 +0000

    Added server

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ if "__main__" == __name__:
     socket.setdefaulttimeout(1)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('0.0.0.0', 1664))
-    s.listen(2)
+    s.listen(100)
     while 1:
         try:
             (cs, addr) = s.accept()
@@ -15,13 +15,30 @@ if "__main__" == __name__:
         except socket.timeout:
             pass
         read, write, err = select.select(players, players, players, 1)
-        for player in read:
-            data = player.recv(4096)
+	for player in err:
             i = players.index(player)
             if(i%2 == 0):
                 try:
-                    players[i+1].send(data)
+                    players.remove(players[i+1])
                 except IndexError:
                     pass
             else:
-                players[i-1].send(data)
+                try:
+                    players.remove(players[i-1])
+		except:
+                   pass
+            players.remove(player)
+            continue
+        try:
+            for player in read:
+                data = player.recv(4096)
+                i = players.index(player)
+                if(i%2 == 0):
+                    try:
+                        players[i+1].send(data)
+                    except IndexError:
+                        pass
+                else:
+                    players[i-1].send(data)
+        except socket.error:
+            print("Lost players")
